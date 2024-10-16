e8d33154a30f54e87e3c500edc21d37e3d2cdbd7
zsanett-molnar <96383347+zsanett-molnar@users.noreply.github.com>
2024-10-16 11:09:01 +0300

    Initial commit

--- frontend/README.md
+++ frontend/README.md
@@ -1,4 +1,4 @@
-# ZsanettWebsite
+# Frontend
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 17.3.1.
 
--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,16 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { MatButtonModule } from '@angular/material/button';
-import { MatSidenavModule } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet, MatButtonModule, MatSidenavModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'zsanett-website';
-  showFiller = false;
+  title = 'frontend';
 }
