7b673222eb8af3383a0e92bfcc0908f9a9fd6c8e
zsanett-molnar <96383347+zsanett-molnar@users.noreply.github.com>
2024-10-18 13:53:17 +0300

    Added active status to buttons

--- frontend/src/app/page/homepage/homepage.component.ts
+++ frontend/src/app/page/homepage/homepage.component.ts
@@ -16,6 +16,7 @@ export class HomepageComponent implements OnInit {
   index: number = 0;
 
   displayedDescription: string = "";
+  selectedDescription: number = 0;
   descriptions: string[] = [
     "During my internship, I worked on two projects simultaneously: one involving an existing company product and another that I developed independently. I was able to apply my knowledge of Java Spring Boot in practice while also working with new technologies such as JavaFX for the frontend. In the second project, which I developed independently, I explored new areas like AI integration and utilized Java Spring Boot-compatible libraries, such as Apache POI.",
     "During my time in the apprentice program at Endava, I had the opportunity to work with a variety of frameworks, technologies, and programming languages. This included SQL data modeling, Java Spring Boot, .NET, Android development (Java/XML), as well as fundamental frontend concepts using JavaScript. This practical experience helped me expand my skill set and gain a deeper understanding of software development practices."
@@ -53,6 +54,7 @@ export class HomepageComponent implements OnInit {
 
   changeDescription(index: number): void {
     this.displayedDescription = this.descriptions[index];
+    this.selectedDescription = index;
   }
 
 }
