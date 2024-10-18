978376fac2fbd961910b71931842acc5f0b1df49
zsanett-molnar <96383347+zsanett-molnar@users.noreply.github.com>
2024-10-18 14:34:28 +0300

    Transition

--- frontend/src/app/page/homepage/homepage.component.ts
+++ frontend/src/app/page/homepage/homepage.component.ts
@@ -17,6 +17,7 @@ export class HomepageComponent implements OnInit {
 
   displayedDescription: string = "";
   selectedDescription: number = 0;
+  descriptionVisible: boolean = false;
   descriptions: string[] = [
     "During my internship, I worked on two projects simultaneously: one involving an existing company product and another that I developed independently. I was able to apply my knowledge of Java Spring Boot in practice while also working with new technologies such as JavaFX for the frontend. In the second project, which I developed independently, I explored new areas like AI integration and utilized Java Spring Boot-compatible libraries, such as Apache POI.",
     "During my time in the apprentice program at Endava, I had the opportunity to work with a variety of frameworks, technologies, and programming languages. This included SQL data modeling, Java Spring Boot, .NET, Android development (Java/XML), as well as fundamental frontend concepts using JavaScript. This practical experience helped me expand my skill set and gain a deeper understanding of software development practices."
@@ -25,8 +26,9 @@ export class HomepageComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    this.displayedDescription = "During my internship, I worked on two projects simultaneously: one involving an existing company product and another that I developed independently. I was able to apply my knowledge of Java Spring Boot in practice while also working with new technologies such as JavaFX for the frontend. In the second project, which I developed independently, I explored new areas like AI integration and utilized Java Spring Boot-compatible libraries, such as Apache POI.";
     this.startTyping();
+    this.descriptionVisible = true;
+    this.displayedDescription = "During my internship, I worked on two projects simultaneously: one involving an existing company product and another that I developed independently. I was able to apply my knowledge of Java Spring Boot in practice while also working with new technologies such as JavaFX for the frontend. In the second project, which I developed independently, I explored new areas like AI integration and utilized Java Spring Boot-compatible libraries, such as Apache POI.";
   }
 
   startTyping(): void {
@@ -53,8 +55,13 @@ export class HomepageComponent implements OnInit {
   }
 
   changeDescription(index: number): void {
-    this.displayedDescription = this.descriptions[index];
     this.selectedDescription = index;
+    this.descriptionVisible = false;
+
+    setTimeout(() => {
+      this.displayedDescription = this.descriptions[index];
+      this.descriptionVisible = true;
+    }, 300);
   }
 
 }
