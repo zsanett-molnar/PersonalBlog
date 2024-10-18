acb03fe3bcc057e31635ddcc476bd59b7ac3e5e9
zsanett-molnar <96383347+zsanett-molnar@users.noreply.github.com>
2024-10-18 13:18:00 +0300

    Updated sections

--- frontend/src/app/page/homepage/homepage.component.ts
+++ frontend/src/app/page/homepage/homepage.component.ts
@@ -15,9 +15,16 @@ export class HomepageComponent implements OnInit {
   typingSpeed: number = 200;
   index: number = 0;
 
+  displayedDescription: string = "";
+  descriptions: string[] = [
+    "During my internship, I worked on two projects simultaneously: one involving an existing company product and another that I developed independently. I was able to apply my knowledge of Java Spring Boot in practice while also working with new technologies such as JavaFX for the frontend. In the second project, which I developed independently, I explored new areas like AI integration and utilized Java Spring Boot-compatible libraries, such as Apache POI.",
+    "During my time in the apprentice program at Endava, I had the opportunity to work with a variety of frameworks, technologies, and programming languages. This included SQL data modeling, Java Spring Boot, .NET, Android development (Java/XML), as well as fundamental frontend concepts using JavaScript. This practical experience helped me expand my skill set and gain a deeper understanding of software development practices."
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
+    this.displayedDescription = "During my internship, I worked on two projects simultaneously: one involving an existing company product and another that I developed independently. I was able to apply my knowledge of Java Spring Boot in practice while also working with new technologies such as JavaFX for the frontend. In the second project, which I developed independently, I explored new areas like AI integration and utilized Java Spring Boot-compatible libraries, such as Apache POI.";
     this.startTyping();
   }
 
@@ -42,6 +49,10 @@ export class HomepageComponent implements OnInit {
     else {
       this.displayedDot += '.';
     }
+  }
+
+  changeDescription(index: number): void {
+    this.displayedDescription = this.descriptions[index];
   }
 
 }
