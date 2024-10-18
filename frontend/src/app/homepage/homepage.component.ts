0a2be83624bb60f3c3a6bc3e653a69ab117ab9d1
zsanett-molnar <96383347+zsanett-molnar@users.noreply.github.com>
2024-10-18 10:31:01 +0300

    Name with different color

---
+++ frontend/src/app/homepage/homepage.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-homepage',
+  templateUrl: './homepage.component.html',
+  styleUrl: './homepage.component.css'
+})
+export class HomepageComponent implements OnInit {
+  fullText: string = 'Hello, this is '
+  name: string = 'Zsanett.';
+  displayText: string = '';
+  displayedName: string = '';
+  typingSpeed: number = 180;
+  index: number = 0;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.startTyping();
+  }
+
+  startTyping(): void {
+    if (this.index < this.fullText.length) {
+      this.displayText += this.fullText.charAt(this.index);
+      this.index++;
+      setTimeout(() => this.startTyping(), this.typingSpeed);
+    }
+    else {
+      this.index = 0;
+      this.typeName();
+    }
+  }
+
+  typeName(): void {
+    if (this.index < this.name.length) {
+      this.displayedName += this.name.charAt(this.index);
+      this.index++;
+      setTimeout(() => this.typeName(), this.typingSpeed);
+    }
+  }
+}
