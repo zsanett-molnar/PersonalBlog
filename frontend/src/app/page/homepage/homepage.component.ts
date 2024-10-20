e4f784cae8f1758efc9e31320be0e8d64499ac5c
zsanett-molnar <96383347+zsanett-molnar@users.noreply.github.com>
2024-10-20 21:04:15 +0300

    Redesinged education section

--- frontend/src/app/page/homepage/homepage.component.ts
+++ frontend/src/app/page/homepage/homepage.component.ts
@@ -23,12 +23,24 @@ export class HomepageComponent implements OnInit {
     "During my time in the apprentice program at Endava, I had the opportunity to work with a variety of frameworks, technologies, and programming languages. This included SQL data modeling, Java Spring Boot, .NET, Android development (Java/XML), as well as fundamental frontend concepts using JavaScript. This practical experience helped me expand my skill set and gain a deeper understanding of software development practices."
   ]
 
+  displayedEducation: string = "";
+  selectedEducation: number = 0;
+  educationVisible: boolean = false;
+  education: string[] = [
+    "Education 1",
+    "Education 2"
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
     this.startTyping();
     this.descriptionVisible = true;
-    this.displayedDescription = "During my internship, I worked on two projects simultaneously: one involving an existing company product and another that I developed independently. I was able to apply my knowledge of Java Spring Boot in practice while also working with new technologies such as JavaFX for the frontend. In the second project, which I developed independently, I explored new areas like AI integration and utilized Java Spring Boot-compatible libraries, such as Apache POI.";
+    this.educationVisible = true;
+    this.displayedEducation = this.education[0];
+    this.displayedDescription = this.descriptions[0];
+    console.log("descript visible: ", this.descriptionVisible);
+    console.log("educaton visible: ", this.educationVisible);
   }
 
   startTyping(): void {
@@ -61,6 +73,16 @@ export class HomepageComponent implements OnInit {
     setTimeout(() => {
       this.displayedDescription = this.descriptions[index];
       this.descriptionVisible = true;
+    }, 300);
+  }
+
+  changeEducation(index: number): void {
+    this.selectedEducation = index;
+    this.educationVisible = false;
+
+    setTimeout(() => {
+      this.displayedEducation = this.education[index];
+      this.educationVisible = true;
     }, 300);
   }
 
